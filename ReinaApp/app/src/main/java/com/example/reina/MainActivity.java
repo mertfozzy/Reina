90423d7e8b22fe103057eabfbc212fd06a488ad7
Mert Altuntaş <mertfozzy@gmail.com>
2022-04-24 10:58:22 +0300

    update

--- ReinaApp/app/src/main/java/com/example/reina/MainActivity.java
+++ ReinaApp/app/src/main/java/com/example/reina/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.reina;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Reina");
     }
 }

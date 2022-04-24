3a588bfc5d5d1af8b54bc901c910fd868bf15cab
Mert Altuntaş <mertfozzy@gmail.com>
2022-04-24 12:31:12 +0300

    chat-groups-contacts tabs

--- ReinaApp/app/src/main/java/com/example/reina/MainActivity.java
+++ ReinaApp/app/src/main/java/com/example/reina/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.reina;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabAccessAdapter myTabAccessAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,13 @@ public class MainActivity extends AppCompatActivity {
         mToolbar = findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Reina Messenger");
+
+        myViewPager = findViewById(R.id.main_tabs_pager);
+        myTabAccessAdapter = new TabAccessAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabAccessAdapter);
+
+        myTabLayout = findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
+
     }
 }
---
+++ ReinaApp/app/src/main/java/com/example/reina/TabAccessAdapter.java
@@ -0,0 +1,59 @@
+package com.example.reina;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabAccessAdapter extends FragmentPagerAdapter {
+    public TabAccessAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                ChatFragment chatFragment = new ChatFragment();
+                return chatFragment;
+
+            case 1:
+                GroupsFragment groupsFragment = new GroupsFragment();
+                return groupsFragment;
+
+            case 2:
+                ContactsFragment contactsFragment = new ContactsFragment();
+                return contactsFragment;
+
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Chats";
+
+            case 1:
+                return "Groups";
+
+            case 2:
+                return "Contacts";
+
+            default:
+                return null;
+        }
+    }
+}

0624f4201068828bd527b023a01b46d62cc4f31c
Mert Altuntaş <mertfozzy@gmail.com>
2022-05-01 18:44:10 +0300

    color fixed

--- ReinaApp/app/src/main/java/com/example/reina/MainActivity.java
+++ ReinaApp/app/src/main/java/com/example/reina/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.reina;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
@@ -56,5 +59,32 @@ public class MainActivity extends AppCompatActivity {
     private void send_user_to_LoginActivity() {
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(loginIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.main_find_friend){
+
+        }
+        if (item.getItemId() == R.id.main_settings){
+
+        }
+        if (item.getItemId() == R.id.main_logout){
+            mAuthentication.signOut();
+            Intent loginScreen = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginScreen);
+        }
+        return true;
     }
 }

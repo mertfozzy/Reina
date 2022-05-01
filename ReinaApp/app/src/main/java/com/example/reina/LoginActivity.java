d4b3dd35b4ccdc2f232be937be0db1a793852fe2
Mert Altuntaş <mertfozzy@gmail.com>
2022-05-01 09:51:42 +0300

    update

--- ReinaApp/app/src/main/java/com/example/reina/LoginActivity.java
+++ ReinaApp/app/src/main/java/com/example/reina/LoginActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginButton, phoneLoginButton;
+    private EditText userMail, userPassword;
+    private TextView signupNew, forgetPassword;
 
     private FirebaseUser existUser;
 
@@ -15,6 +23,26 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //controls
+        loginButton = findViewById(R.id.login_button);
+        phoneLoginButton = findViewById(R.id.another_login_button);
+
+        userMail = findViewById(R.id.login_mail);
+        userPassword = findViewById(R.id.login_password);
+
+        signupNew = findViewById(R.id.signup_new);
+        forgetPassword = findViewById(R.id.forget_password);
+
+        signupNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signupActivityIntent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(signupActivityIntent);
+            }
+        });
+
+
     }
 
     @Override
--- ReinaApp/app/src/main/java/com/example/reina/SignupActivity.java
+++ ReinaApp/app/src/main/java/com/example/reina/SignupActivity.java
@@ -2,13 +2,39 @@ package com.example.reina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private Button createAccountButton;
+    private EditText userMail, userPassword;
+    private TextView alreadyHaveAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        //controls
+        createAccountButton = findViewById(R.id.signup_button);
+
+        userMail = findViewById(R.id.signup_mail);
+        userPassword = findViewById(R.id.signup_password);
+
+        alreadyHaveAccount = findViewById(R.id.login_instead);
+
+        alreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginActivityIntent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(loginActivityIntent);
+            }
+        });
+
     }
 }

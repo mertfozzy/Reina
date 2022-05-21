82b86433bd09ed3172098b3db9f8d9a00443e21b
Mert Altuntaş <mertfozzy@gmail.com>
2022-05-21 10:11:22 +0300

    update

--- ReinaApp/app/src/main/java/com/example/reina/MainActivity.java
+++ ReinaApp/app/src/main/java/com/example/reina/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.reina;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -127,6 +133,55 @@ public class MainActivity extends AppCompatActivity {
             Intent loginScreen = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(loginScreen);
         }
+
+        if (item.getItemId() == R.id.main_create_group){
+            newGroupRequest();
+        }
         return true;
+    }
+
+    private void newGroupRequest() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this, R.style.AlertDialog);
+        builder.setTitle("Enter a Group Name: ");
+        final EditText groupNameField = new EditText(MainActivity.this);
+        groupNameField.setHint("Example : Reina Developers");
+        builder.setView(groupNameField);
+
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String groupName = groupNameField.getText().toString();
+
+                if (TextUtils.isEmpty(groupName)){
+                    Toast.makeText(MainActivity.this, "Group name cannot be empty.", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    CreateNewGroup(groupName);
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void CreateNewGroup(String groupName) {
+        usersReference.child("Groups").child(groupName).setValue("").addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+                if (task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, groupName + " created successfully.", Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
     }
 }

9989454078b5fc0c0aaedf2a777198e45ec673d8
Mert Altuntaş <mertfozzy@gmail.com>
2022-06-05 17:49:56 +0300

    update

--- ReinaApp/app/src/main/java/com/example/reina/profileActivity.java
+++ ReinaApp/app/src/main/java/com/example/reina/profileActivity.java
@@ -26,7 +26,7 @@ public class profileActivity extends AppCompatActivity {
 
     private CircleImageView userProfilePhoto;
     private TextView userProfileName, userProfileAbout;
-    private Button sendMessageButton;
+    private Button sendMessageButton, evaluationButton;
 
     //firebase
     private DatabaseReference userPath, chatRequestPath;
@@ -45,6 +45,7 @@ public class profileActivity extends AppCompatActivity {
         userProfileName = findViewById(R.id.username_visit);
         userProfileAbout = findViewById(R.id.about_visit);
         sendMessageButton = findViewById(R.id.message_sent_request);
+        evaluationButton = findViewById(R.id.message_request_evaluation);
 
         activeStatus = "new";
 
@@ -92,6 +93,45 @@ public class profileActivity extends AppCompatActivity {
 
     private void manageChatRequest() {
 
+        //if there is a request, show cancel button
+        chatRequestPath.child(activeUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if (snapshot.hasChild(fetchedUserID)){
+                    String requestType = snapshot.child(fetchedUserID).child("request_type").getValue().toString();
+
+                    if (requestType.equals("sent")){
+
+                        activeStatus = "request_sent";
+                        sendMessageButton.setText("Cancel Chat Request");
+
+                    }
+
+                    else{
+
+                        activeStatus = "request_received";
+                        evaluationButton.setText("Accept the Chat Request");
+                        evaluationButton.setVisibility(View.VISIBLE);
+                        evaluationButton.setEnabled(true);
+                        evaluationButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                cancelChatRequest();
+                            }
+                        });
+
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         if (activeUserID.equals(fetchedUserID)){
             //hide button (user cannot send message to himself)
             sendMessageButton.setVisibility(View.INVISIBLE);
@@ -103,12 +143,49 @@ public class profileActivity extends AppCompatActivity {
                 public void onClick(View view) {
 
                     sendMessageButton.setEnabled(false);
+
                     if (activeStatus.equals("new")){
                         sendChatRequest();
+                    }
+                    if (activeStatus.equals("request_sent")){
+                        cancelChatRequest();
                     }
                 }
             });
         }
+
+    }
+
+    private void cancelChatRequest() {
+
+        chatRequestPath.child(activeUserID).child(fetchedUserID).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+                if (task.isSuccessful()){
+                    chatRequestPath.child(fetchedUserID).child(activeUserID).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if (task.isSuccessful()){
+
+                                sendMessageButton.setEnabled(true);
+                                activeStatus = "new";
+                                sendMessageButton.setText("Send a Chat Request");
+
+                                evaluationButton.setVisibility(View.INVISIBLE);
+                                evaluationButton.setEnabled(false);
+
+                            }
+
+                        }
+                    });
+                }
+
+            }
+        });
+
+
 
     }
 
